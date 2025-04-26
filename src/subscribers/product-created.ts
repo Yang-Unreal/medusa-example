2859470f4bc5d76386bedee1ae5220dac79d2cd5
Yang <y953159141@gmail.com>
2025-04-26 14:38:59 +0800

    Update product-created.ts

--- src/subscribers/product-created.ts
+++ src/subscribers/product-created.ts
@@ -1,45 +1,44 @@
-// import type { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
-// import { Modules } from "@medusajs/framework/utils";
-// import { Logger } from "@medusajs/framework/types";
-
-// export default async function productCreatedHandler({
-//   event: { data },
-//   container,
-// }: SubscriberArgs<{ id: string }>) {
-//   const logger = container.resolve<Logger>("logger");
-
-//   logger.debug("🟢 Event received");
-
-//   const notificationModuleService = container.resolve(Modules.NOTIFICATION);
-//   const productModuleService = container.resolve(Modules.PRODUCT);
-
-//   const product = await productModuleService.retrieveProduct(data.id);
-//   await notificationModuleService.createNotifications({
-//     to: "y953159141@gmail.com",
-//     channel: "email",
-//     template: "product-created",
-//     data: {
-//       product_title: product.title,
-//       product_image: product.images[0]?.url,
-//     },
-//   });
-// }
-// export const config: SubscriberConfig = {
-//   event: "product.created",
-// };
-
-// subscriber function
 import type { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
+import { Modules } from "@medusajs/framework/utils";
 
-export default async function productCreateHandler({
-  event,
+export default async function productCreatedHandler({
+  event: { data },
+  container,
 }: SubscriberArgs<{ id: string }>) {
-  const productId = event.data.id;
-  console.log(
-    `The product ${productId} was created-------------------------------------------------------------------`
-  );
-}
+  console.log("🟢 Event received");
 
+  const notificationModuleService = container.resolve(Modules.NOTIFICATION);
+  const productModuleService = container.resolve(Modules.PRODUCT);
+
+  const product = await productModuleService.retrieveProduct(data.id);
+  await notificationModuleService.createNotifications({
+    to: "y953159141@gmail.com",
+    channel: "email",
+    template: "product-created",
+    data: {
+      product_title: product.title,
+      product_image: product.images[0]?.url,
+    },
+  });
+}
 export const config: SubscriberConfig = {
   event: "product.created",
 };
+
+// import type { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
+
+// console.log(
+//   `---------------------------------------The subscriber was init---------------------------------------`
+// );
+// export default async function productCreateHandler({
+//   event,
+// }: SubscriberArgs<{ id: string }>) {
+//   const productId = event.data.id;
+//   console.log(
+//     `---------------------------------------The product ${productId} was created---------------------------------------`
+//   );
+// }
+
+// export const config: SubscriberConfig = {
+//   event: "product.created",
+// };
